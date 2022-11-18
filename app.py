fdb0bdd719bd4c3426182d6aac48b912d85b2b07
Harikrishnan D <harideegee@gmail.com>
2022-11-18 09:43:46 +0530

    made changes to the edit feature

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ while True:
     user_action = user_action.lower()
     user_action = user_action.strip()
 
-    if "add" in user_action:
+    if user_action.startswith("add"):
         todo = user_action[4:] + "\n"
 
         with open("user_data.txt", "r") as file:
@@ -38,7 +38,7 @@ while True:
         todo_wo_break = todo.strip('\n')
         print(f"The item '{todo_wo_break}' was added to your to do list. You now have {len(todos)} thing(s) left to do.")
     
-    elif "show" in user_action:
+    elif user_action.startswith("show"):
         with open("user_data.txt", "r") as file:
             todos = file.readlines()
         for index, item in enumerate(todos):
@@ -46,28 +46,51 @@ while True:
             print(f"{index+1}. {item_wo_break}")
         print(f"You have {len(todos)} thing(s) left to do.")
     
-    elif "edit" in user_action:
+    elif user_action.startswith("edit"):
         for index, item in enumerate(todos):
             item_wo_break = item.strip('\n')
             print(f"{index+1}. {item_wo_break}")
-        edit_position = int(user_action[5:])
-        edit_position = edit_position - 1
-        todo_wo_break = todos[edit_position].strip('\n')
-        edited_todo = input(f"What do you want to replace '{todo_wo_break}' with? ") + "\n"
+        try:
+            edit_position = int(user_action[5:])
+            edit_position = edit_position - 1
+            todo_wo_break = todos[edit_position].strip('\n')
+            edited_todo = input(f"What do you want to replace '{todo_wo_break}' with? ") + "\n"
 
-        with open("user_data.txt", "r") as file:
-            todos = file.readlines()
-            previous_value = todos[edit_position]
-            todos[edit_position] = edited_todo
-        
-        with open("user_data.txt", "w") as file:
-            file.writelines(todos)
+            with open("user_data.txt", "r") as file:
+                todos = file.readlines()
+                previous_value = todos[edit_position]
+                todos[edit_position] = edited_todo
+            
+            with open("user_data.txt", "w") as file:
+                file.writelines(todos)
 
-        todo_wo_break = edited_todo.strip('\n')
-        prev_wo_break = previous_value.strip('\n')
-        print(f"The item '{prev_wo_break}' was updated to '{todo_wo_break}'!")
+            todo_wo_break = edited_todo.strip('\n')
+            prev_wo_break = previous_value.strip('\n')
+            print(f"The item '{prev_wo_break}' was updated to '{todo_wo_break}'!")
+        except:
+            with open("user_data.txt", "r") as file:
+                todos = file.readlines()
+            data_verify = user_action[5:] + '\n'
+            if data_verify in todos:
+                todo_wo_break = user_action[5:].strip('\n')
+                edited_todo = input(f"What do you want to replace '{todo_wo_break}' with? ") + "\n"
+                edit_position = todos.index(data_verify)
+                with open("user_data.txt", "r") as file:
+                    todos = file.readlines()
+                    previous_value = todo_wo_break
+                    todos[edit_position] = edited_todo
+                
+                with open("user_data.txt", "w") as file:
+                    file.writelines(todos)
+
+                todo_wo_break = edited_todo.strip('\n')
+                prev_wo_break = previous_value.strip('\n')
+                print(f"The item '{prev_wo_break}' was updated to '{todo_wo_break}'!")
+            else:
+                print("Did not find that task in your list. Consider using the 'add' command.")
+
     
-    elif "complete" in user_action:
+    elif user_action.startswith("complete"):
         try:
             complete = int(user_action[9:])
             complete = complete - 1
@@ -92,7 +115,7 @@ while True:
             print(f"All the tasks were removed from your list! You now have {len(todos)} thing(s) left to do.")
         
     
-    elif "exit" in user_action:
+    elif user_action.startswith("exit"):
         break
     
     else:
